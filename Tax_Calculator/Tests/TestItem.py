d3866f12705e06b02ffde361ce44b69d80951802
RomanGressler <gressler@uni-bremen.de>
2022-05-30 00:22:02 +0200

    added more tests

--- Tax_Calculator/Tests/TestItem.py
+++ Tax_Calculator/Tests/TestItem.py
@@ -4,16 +4,22 @@ from item import *
 
 class ItemTestCase(unittest.TestCase):
     def test_tax_calculator(self):
-        game = Item("Game", Item_type.DEFAULT, 10, False, 2)
-        med = Item("Med", Item_type.MEDICAL, 100, False, 1)
-        apple = Item("Apple", Item_type.FOOD, 1, False, 1)
-        book = Item("Book", Item_type.BOOK, 1, False, 1)
-        imported_book = Item("Imported Book", Item_type.BOOK, 1, True, 1)
+        game = Item("Game", ItemType.DEFAULT, 10, False, 2)
+        med = Item("Med", ItemType.MEDICAL, 100, False, 1)
+        apple = Item("Apple", ItemType.FOOD, 1, False, 1)
+        book = Item("Book", ItemType.BOOK, 1, False, 1)
+        imported_book = Item("Imported Book", ItemType.BOOK, 1, True, 1)
         self.assertEqual(game.calc_tax(), 2)
         self.assertEqual(med.calc_tax(), 0)
         self.assertEqual(apple.calc_tax(), 0)
         self.assertEqual(book.calc_tax(), 0)
         self.assertEqual(imported_book.calc_tax(), 0.05)
+
+    def test_tax_rounding(self):
+        bottle = Item("imported bottle of perfume", ItemType.DEFAULT, 47.5, True, 1)
+        self.assertEqual(bottle.round_tax(bottle.price * 0.15), 7.15)
+        bottle2 = Item("imported bottle of perfume", ItemType.DEFAULT, 27.99, True, 1)
+        self.assertEqual(bottle2.round_tax(bottle2.price * 0.15), 4.2)
 
 
 if __name__ == '__main__':
