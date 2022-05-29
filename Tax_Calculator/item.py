badc84fdf9892557c0410b319d18c00d99e41bfd
RomanGressler <gressler@uni-bremen.de>
2022-05-30 00:23:05 +0200

    added tax rounding

--- Tax_Calculator/item.py
+++ Tax_Calculator/item.py
@@ -1,4 +1,6 @@
-from item_type import *
+import math
+
+from itemtype import *
 
 
 class Item:
@@ -11,14 +13,18 @@ class Item:
 
     def calc_tax(self):
         """Calculate the tax of an item. Checks if the item is imported and in which tax group it is"""
-        if self.type == Item_type.BOOK:
+        if self.type == ItemType.BOOK:
             tax = 0
-        elif self.type == Item_type.MEDICAL:
+        elif self.type == ItemType.MEDICAL:
             tax = 0
-        elif self.type == Item_type.FOOD:
+        elif self.type == ItemType.FOOD:
             tax = 0
         else:
             tax = 0.1
         if self.imported:
             tax += 0.05
-        return self.price * tax * self.amount
+        tax_rounded = self.round_tax(float(self.price) * float(tax))
+        return tax_rounded * float(self.amount)
+
+    def round_tax(self, value):
+        return math.ceil(value / 0.05) * 0.05
